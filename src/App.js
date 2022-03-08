83b16b3908f623bb10b8ff0677ae89168c94b1da
MJRudolph <ReindeerCode@gmail.com>
2022-03-07 19:39:32 -0500

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./Pages/LandingPage";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
