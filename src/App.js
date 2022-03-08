4c5eb743aa50bd78d70c335344089e8dc388f125
MJRudolph <ReindeerCode@gmail.com>
2022-03-07 19:52:50 -0500

    changed Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/landing";
 
 export default function App() {
   return (
     <>
       <Router>
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Landing} />
-        </Switch>
+        </Routes>
       </Router>
     </>
   );
